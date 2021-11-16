c6ad7bfa941b18065a9ccc8156e01f885a796970
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-11-16 14:00:59 +0400

    validations

--- CustomerApplication/src/CustomerApp/Customer/CustomerApp.CustomerModule.ts
+++ CustomerApplication/src/CustomerApp/Customer/CustomerApp.CustomerModule.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule} from '@angular/router';
 import { CustomerRoutes } from '../Routing/CustomerApp.CustomerRouting';
@@ -13,7 +13,8 @@ import { CustomerComponent } from './CustomerApp.CustomerComponent';
   imports: [
     RouterModule.forChild(CustomerRoutes),
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [CustomerComponent]
--- CustomerApplication/src/CustomerApp/Customer/CustomerApp.model.ts
+++ CustomerApplication/src/CustomerApp/Customer/CustomerApp.model.ts
@@ -1,5 +1,16 @@
+import { NgForm,FormGroup,FormControl,Validators,FormBuilder } from "@angular/forms";
+
 export class Customer{
     CustomerCode:string="";
     CustomerName:string="";
     CustomerAmount:number=0;
+    formCutomerGroup: FormGroup;
+
+    constructor(){
+        var _builder=new FormBuilder();
+        this.formCutomerGroup=_builder.group({});
+        //control-->validations
+        this.formCutomerGroup.addControl("CustomerNameControl",new FormControl('',Validators.required));
+        
+    }
 }
